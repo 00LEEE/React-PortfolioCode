90e21d68b2e7f714fcb9e1f5ccee60c3de0b670d
Kole Dallam <kolejasonD@gmail.com>
2023-05-10 22:17:29 -0600

    syntax changes and optimization

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,8 +1,5 @@
 import React from "react";
-import {
-  VerticalTimeline,
-  VerticalTimelineElement,
-} from "react-vertical-timeline-component";
+import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 import SchoolIcon from "@mui/icons-material/School";
 import WorkIcon from "@mui/icons-material/Work";
@@ -20,7 +17,7 @@ function Experience() {
           <h3 className="vertical-timeline-element-title">
             Gunnison Valley High, Gunnison, UT
           </h3>
-          <p> High School Diploma</p>
+          <p>High School Diploma</p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--education"
@@ -31,33 +28,30 @@ function Experience() {
           <h3 className="vertical-timeline-element-title">
             Snow College, Ephraim, UT
           </h3>
-
           <h4 className="vertical-timeline-element-subtitle">
             General Studies
           </h4>
-
           <p>Emphasis in Fine Arts (Graphic Design/3D Modeling)</p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
           date="2019 - 2020"
           iconStyle={{ background: "#3e497a", color: "#fff" }}
-          icon={<SchoolIcon />}
+          icon={<WorkIcon />}
         >
           <h3 className="vertical-timeline-element-title">
             Salt Lake Community, South SLC, UT
           </h3>
           <h4 className="vertical-timeline-element-subtitle">
-            Associates Degree
+            Associate's Degree
           </h4>
-          <p>Studied 3D modeling/Annimation/Software Engineering </p>
+          <p>Studied 3D modeling/Animation/Software Engineering</p>
         </VerticalTimelineElement>
-
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
           date="2022 - 2023"
           iconStyle={{ background: "#3e497a", color: "#fff" }}
-          icon={<SchoolIcon />}
+          icon={<WorkIcon />}
         >
           <h3 className="vertical-timeline-element-title">
             University of Utah, SLC, UT
@@ -66,13 +60,12 @@ function Experience() {
             Full Stack Web Developer Bootcamp
           </h4>
           <p>
-            During my comprehensive full stack web development bootcamp, I
-            gained expertise in front-end development technologies such as HTML,
-            CSS, and JavaScript, as well as back-end development with Node.js
-            and Django frameworks. I also acquired knowledge of effective
-            database design and management practices. Through hands-on
-            experience and collaborative projects, I learned how to build
-            dynamic web applications from concept to completion.
+            During my comprehensive full stack web development bootcamp, I gained 
+            expertise in front-end development technologies such as HTML, CSS, and JavaScript,
+            as well as back-end development with Node.js and Django frameworks.
+            I also acquired knowledge of effective database design and management practices. 
+            Through hands-on experience and collaborative projects, I learned how to build dynamic web applications 
+            from concept to completion.
           </p>
         </VerticalTimelineElement>
       </VerticalTimeline>
@@ -81,3 +74,4 @@ function Experience() {
 }
 
 export default Experience;
+
