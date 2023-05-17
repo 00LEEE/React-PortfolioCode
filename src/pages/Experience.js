96bf98a4475676b7dc63deebabe89aa0ddc98e69
Kole Dallam <kolejasonD@gmail.com>
2023-05-16 19:55:11 -0600

    timeline revamp

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,78 +1,143 @@
-import React from "react";
-import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
+import React, { useState, useEffect } from "react";
 import "react-vertical-timeline-component/style.min.css";
 import SchoolIcon from "@mui/icons-material/School";
 import WorkIcon from "@mui/icons-material/Work";
 
 function Experience() {
+  const [experiences, setExperiences] = useState([
+    {
+      date: "2013 - 2017",
+      title: "Gunnison Valley High, Gunnison, UT",
+      subtitle: "",
+      description: "High School Diploma",
+      icon: <SchoolIcon />,
+    },
+    {
+      date: "2017 - 2019",
+      title: "Snow College, Ephraim, UT",
+      subtitle: "General Studies",
+      description: "Emphasis in Fine Arts (Graphic Design/3D Modeling)",
+      icon: <SchoolIcon />,
+    },
+    {
+      date: "2019 - 2020",
+      title: "Salt Lake Community, South SLC, UT",
+      subtitle: "Associate's Degree",
+      description: "Studied 3D modeling/Animation/Software Engineering",
+      icon: <SchoolIcon />,
+    },
+    {
+      date: "2022 - 2023",
+      title: "University of Utah, SLC, UT",
+      subtitle: "Full Stack Web Developer Bootcamp",
+      description:
+        "During my comprehensive full stack web development bootcamp, I gained expertise in front-end development technologies such as HTML, CSS, and JavaScript, as well as back-end development with Node.js and Django frameworks. I also acquired knowledge of effective database design and management practices. Through hands-on experience and collaborative projects, I learned how to build dynamic web applications from concept to completion.",
+      icon: <SchoolIcon />,
+    },
+  ]);
+
+  useEffect(() => {
+    const experienceCards = Array.from(document.querySelectorAll(".experience-card"));
+    experienceCards.forEach((card, index) => {
+      card.classList.add("bounce-in");
+    });
+
+    return () => {
+      experienceCards.forEach((card, index) => {
+        card.classList.remove("bounce-in");
+      });
+    };
+  }, []);
+
   return (
     <div className="experience">
-      <VerticalTimeline lineColor="#3e497a">
-        <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          date="2013 - 2017"
-          iconStyle={{ background: "#3e497a", color: "#fff", boxShadow: '0px 0px 10px 2px rgba(0, 0, 0, 0.6)' }}
-          icon={<SchoolIcon />}
-        >
-          <h3 className="vertical-timeline-element-title">
-            Gunnison Valley High, Gunnison, UT
-          </h3>
-          <p>High School Diploma</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          date="2017 - 2019"
-          iconStyle={{ background: "#3e497a", color: "#fff", boxShadow: '0px 0px 10px 2px rgba(0, 0, 0, 0.6)' }}
-          icon={<SchoolIcon />}
-        >
-          <h3 className="vertical-timeline-element-title">
-            Snow College, Ephraim, UT
-          </h3>
-          <h4 className="vertical-timeline-element-subtitle">
-            General Studies
-          </h4>
-          <p>Emphasis in Fine Arts (Graphic Design/3D Modeling)</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          date="2019 - 2020"
-          iconStyle={{ background: "#3e497a", color: "#fff", boxShadow: '0px 0px 10px 2px rgba(0, 0, 0, 0.6)' }}
-          icon={<WorkIcon />}
-        >
-          <h3 className="vertical-timeline-element-title">
-            Salt Lake Community, South SLC, UT
-          </h3>
-          <h4 className="vertical-timeline-element-subtitle">
-            Associate's Degree
-          </h4>
-          <p>Studied 3D modeling/Animation/Software Engineering</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          date="2022 - 2023"
-          iconStyle={{ background: "#3e497a", color: "#fff", boxShadow: '0px 0px 10px 2px rgba(0, 0, 0, 0.6)' }}
-          icon={<WorkIcon />}
-        >
-          <h3 className="vertical-timeline-element-title">
-            University of Utah, SLC, UT
-          </h3>
-          <h4 className="vertical-timeline-element-subtitle">
-            Full Stack Web Developer Bootcamp
-          </h4>
-          <p>
-            During my comprehensive full stack web development bootcamp, I gained 
-            expertise in front-end development technologies such as HTML, CSS, and JavaScript,
-            as well as back-end development with Node.js and Django frameworks.
-            I also acquired knowledge of effective database design and management practices. 
-            Through hands-on experience and collaborative projects, I learned how to build dynamic web applications 
-            from concept to completion.
-          </p>
-        </VerticalTimelineElement>
-      </VerticalTimeline>
-    </div>
-  );
-}
+      <style>{`
+        .experience {
+          padding: 20px;
+        }
 
-export default Experience;
+        .experience-cards {
+          display: flex;
+          flex-wrap: wrap;
+          justify-content: center;
+        }
 
+        .experience-card {
+          width: 300px;
+          background: linear-gradient(to bottom right, #b6b171, #b6b171);
+          color: #fff;
+          margin: 10px;
+          border-radius: 8px;
+          box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.2);
+          animation: bounce-in 1s ease-out;
+        }
+
+        .experience-card-header {
+          display: flex;
+          align-items: center;
+          padding: 10px;
+        }
+
+        .experience-card-icon {
+          font-size: 30px;
+          margin-right: 10px;
+        }
+
+        .experience-card-date {
+          font-weight: bold;
+        }
+
+        .experience-card-content {
+          padding: 10px;
+        }
+
+        .experience-card-title {
+          margin: 0;
+          font-size: 18px;
+        }
+
+        .experience-card-subtitle {
+          margin: 5px 0;
+          font-size: 14px;
+          font-weight: bold;
+        }
+
+        .experience-card-description {
+          margin: 0;
+        }
+
+        @keyframes bounce-in {
+          0% {
+            opacity: 0;
+            transform: translateY(-50px);
+          }
+          100% {
+            opacity: 1;
+            transform: translateY(0);
+          }
+        }
+              `}</style>
+        
+              <div className="experience-cards">
+                {experiences.map((experience, index) => (
+                  <div key={index} className="experience-card">
+                    <div className="experience-card-header">
+                      <div className="experience-card-icon">{experience.icon}</div>
+                      <div className="experience-card-date">{experience.date}</div>
+                    </div>
+                    <div className="experience-card-content">
+                      <h3 className="experience-card-title">{experience.title}</h3>
+                      {experience.subtitle && (
+                        <h4 className="experience-card-subtitle">{experience.subtitle}</h4>
+                      )}
+                      <p className="experience-card-description">{experience.description}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          );
+        }
+        
+        export default Experience;
 
