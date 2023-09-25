f0f773d5bf1c1bb16acb4fb023dd0a1bf95c7a8d
Kole Dallam <kolejasonD@gmail.com>
2023-09-25 16:17:18 -0600

    adding footer changes to react portfolio.

---
+++ README.md
@@ -0,0 +1,57 @@
+# React-Portfolio📜
+ - Built a React portfolio as a good intro to my experience using react. Very different and very versitile comparably speaking of course. Personally I found that making this using react was a whole lot easier than doing it in HTML & JS. I didnt have to write near as many lines of code as I did with my first portfolio.
+
+# Author ✏️
+- Kole Dallam
+
+# Built Using 🚧
+- HTML 
+- CSS 
+- Figma
+- React
+- JavaScript 
+
+# Trials I Experienced 🤬
+- Making sure the Imports on every page were input correctly. 
+- Creating the Virtical Timeline was a little difficult; I have never attempted a design method such as that.
+- Embedding links inside each of the social icons as well as the Email icon specifically; that one was a little more difficult as I have never linked my email anywhere other than a "Contact Me" section on my normal portfolio using HTML & JS. Ultimately I got things done and it turned out really well I think.
+
+![Alt text](https://i.imgur.com/S7qxxUW.png)
+
+Deployed website on Github: https://00leee.github.io/React-Portfolio/
+*Note - Had to create seperate repo for publishing on Github Pages please visit: "https://github.com/00LEEE/React-PortfolioCode" for code.
+
+# Continued Devolopment:
+- Add links to all my projects not just placholders. Some of my projects are broken right now and need a little fixing so I decided to wait on them until I fix them.
+- Add more experience as I gain it along with continued education if I take any other classes/courses.
+
+## Available Scripts if forking repo
+
+!!MAKE SURE TO INSTALL DEPENDENCIES!!
+
+### 'npm install'
+
+In the project directory, you can run:
+
+### `npm run start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+The page will reload when you make changes.\
+You may also see any lint errors in the console.
+
+### `npm test`
+
+Launches the test runner in the interactive watch mode.
+
+### `npm run build`
+
+Builds the app for production to the `build` folder.\
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.\
+Your app is ready to be deployed!
+
+# Licensed
+- This project is Licensed under the MIT License.
---
+++ src/pages/Experience.js
@@ -0,0 +1,143 @@
+import React, { useState, useEffect } from "react";
+import "react-vertical-timeline-component/style.min.css";
+import SchoolIcon from "@mui/icons-material/School";
+import WorkIcon from "@mui/icons-material/Work";
+
+function Experience() {
+  const [experiences, setExperiences] = useState([
+    {
+      date: "2013 - 2017",
+      title: "Gunnison Valley High, Gunnison, UT",
+      subtitle: "",
+      description: "High School Diploma",
+      icon: <SchoolIcon />,
+    },
+    {
+      date: "2017 - 2019",
+      title: "Snow College, Ephraim, UT",
+      subtitle: "General Studies",
+      description: "Emphasis in Fine Arts (Graphic Design/3D Modeling)",
+      icon: <SchoolIcon />,
+    },
+    {
+      date: "2019 - 2020",
+      title: "Salt Lake Community, South SLC, UT",
+      subtitle: "Associate's Degree",
+      description: "Studied 3D modeling/Animation/Software Engineering",
+      icon: <SchoolIcon />,
+    },
+    {
+      date: "2022 - 2023",
+      title: "University of Utah, SLC, UT",
+      subtitle: "Full Stack Web Developer Bootcamp",
+      description:
+        "During my comprehensive full stack web development bootcamp, I gained expertise in front-end development technologies such as HTML, CSS, and JavaScript, as well as back-end development with Node.js and Django frameworks. I also acquired knowledge of effective database design and management practices. Through hands-on experience and collaborative projects, I learned how to build dynamic web applications from concept to completion.",
+      icon: <SchoolIcon />,
+    },
+  ]);
+
+  useEffect(() => {
+    const experienceCards = Array.from(document.querySelectorAll(".experience-card"));
+    experienceCards.forEach((card, index) => {
+      card.classList.add("bounce-in");
+    });
+
+    return () => {
+      experienceCards.forEach((card, index) => {
+        card.classList.remove("bounce-in");
+      });
+    };
+  }, []);
+
+  return (
+    <div className="experience" style={{ minHeight: 'calc(100vh - 200px)' }}>
+      <style>{`
+        .experience {
+          padding: 20px;
+        }
+
+        .experience-cards {
+          display: flex;
+          flex-wrap: wrap;
+          justify-content: center;
+        }
+
+        .experience-card {
+          width: 300px;
+          background: linear-gradient(to bottom right, #b6b171, #b6b171);
+          color: #fff;
+          margin: 10px;
+          border-radius: 8px;
+          box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.2);
+          animation: bounce-in 1s ease-out;
+        }
+
+        .experience-card-header {
+          display: flex;
+          align-items: center;
+          padding: 10px;
+        }
+
+        .experience-card-icon {
+          font-size: 30px;
+          margin-right: 10px;
+        }
+
+        .experience-card-date {
+          font-weight: bold;
+        }
+
+        .experience-card-content {
+          padding: 10px;
+        }
+
+        .experience-card-title {
+          margin: 0;
+          font-size: 18px;
+        }
+
+        .experience-card-subtitle {
+          margin: 5px 0;
+          font-size: 14px;
+          font-weight: bold;
+        }
+
+        .experience-card-description {
+          margin: 0;
+        }
+
+        @keyframes bounce-in {
+          0% {
+            opacity: 0;
+            transform: translateY(-50px);
+          }
+          100% {
+            opacity: 1;
+            transform: translateY(0);
+          }
+        }
+              `}</style>
+        
+              <div className="experience-cards">
+                {experiences.map((experience, index) => (
+                  <div key={index} className="experience-card">
+                    <div className="experience-card-header">
+                      <div className="experience-card-icon">{experience.icon}</div>
+                      <div className="experience-card-date">{experience.date}</div>
+                    </div>
+                    <div className="experience-card-content">
+                      <h3 className="experience-card-title">{experience.title}</h3>
+                      {experience.subtitle && (
+                        <h4 className="experience-card-subtitle">{experience.subtitle}</h4>
+                      )}
+                      <p className="experience-card-description">{experience.description}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          );
+        }
+        
+        export default Experience;
+
